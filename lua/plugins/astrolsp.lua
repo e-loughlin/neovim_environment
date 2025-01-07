34898ed75f798a212285e08ded4828a115d78ec8
Evan Loughlin <evan@neara.com>
2025-01-07 18:37:11 +1100

    Got Molten and Quarto working for Jupyter notebooks

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -31,48 +31,48 @@ return {
         -- "lua_ls",
       },
       timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
     },
 
     -- enable servers that you already have installed without mason
     servers = {
-      -- "pyright",
+      -- "pyright", -- Uncomment this if you're not using Mason to manage the `pyright` server
     },
+
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      pyright = {
+        on_attach = function(client, bufnr)
+          -- Your custom on_attach function can be added here
+        end,
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        settings = {
+          python = {
+            analysis = {
+              diagnosticSeverityOverrides = {
+                reportUnusedExpression = "none",
+              },
+            },
+          },
+        },
+      },
       clangd = {
         cmd = { "clangd", "--compile-commands-dir=/Users/eloughlin/ws/compile_commands/compile_commands_fea.json" },
       },
     },
+
     -- customize how language servers are attached
     handlers = {
       -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
-      -- function(server, opts) require("lspconfig")[server].setup(opts) end
       dartls = function(_, opts) require("lspconfig").dartls.setup(opts) end,
-
-      -- the key is the server that is being setup with `lspconfig`
-      -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
-      -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
     },
+
     -- Configure buffer local auto commands to add when attaching a language server
     autocmds = {
-      -- first key is the `augroup` to add the auto commands to (:h augroup)
       lsp_codelens_refresh = {
-        -- Optional condition to create/delete auto command group
-        -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
-        -- condition will be resolved for each client on each execution and if it ever fails for all clients,
-        -- the auto commands will be deleted for that buffer
         cond = "textDocument/codeLens",
-        -- cond = function(client, bufnr) return client.name == "lua_ls" end,
-        -- list of auto commands to set
         {
-          -- events to trigger
           event = { "InsertLeave", "BufEnter" },
-          -- the rest of the autocmd options (:h nvim_create_autocmd)
           desc = "Refresh codelens (buffer)",
           callback = function(args)
             if require("astrolsp").config.features.codelens then vim.lsp.codelens.refresh { bufnr = args.buf } end
@@ -80,10 +80,10 @@ return {
         },
       },
     },
+
     -- mappings to be set up on attaching of a language server
     mappings = {
       n = {
-        -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
         gD = {
           function() vim.lsp.buf.declaration() end,
           desc = "Declaration of current symbol",
@@ -98,12 +98,10 @@ return {
         },
       },
     },
+
     -- A custom `on_attach` function to be run after the default `on_attach` function
-    -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
     on_attach = function(client, bufnr)
-      -- print("Attached LSP:", client.name)
-      -- this would disable semanticTokensProvider for all clients
-      -- client.server_capabilities.semanticTokensProvider = nil
+      -- Additional customization for on_attach can be placed here
     end,
   },
 }
