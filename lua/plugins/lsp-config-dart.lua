a6ba151440fe1a2c578916f3af2e75b1f0bfc561
Evan Loughlin <evan@neara.com>
2025-01-07 14:42:00 +1100

    2025-01-07_14:42:00

--- lua/plugins/lsp-config-dart.lua
+++ lua/plugins/lsp-config-dart.lua
@@ -11,13 +11,12 @@ return {
       local bufopts = { noremap = true, silent = true, buffer = bufnr }
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
       vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-      vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, bufopts)
-      vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, bufopts)
+      vim.keymap.set("n", "<leader>lw", vim.lsp.buf.workspace_symbol, bufopts)
+      vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, bufopts)
       vim.keymap.set("n", "[d", vim.diagnostic.goto_next, bufopts)
       vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, bufopts)
-      vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, bufopts)
-      vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, bufopts)
-      vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, bufopts)
+      vim.keymap.set("n", "<leader>lR", vim.lsp.buf.references, bufopts)
+      vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, bufopts)
       vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, bufopts)
     end
 
